e8cdf7c84ca933a3f6f125b1b8e6a75f948863d7
apple <bronzels@hotmail.com>
2022-11-29 10:50:04 +0800

    1，增加kubesphere安装。（containerd/docker都安装失败，而且无法彻底卸载etcd无法卸载干净。）2，优化dd备份恢复。

--- native/backup.sh
+++ native/backup.sh
@@ -19,9 +19,12 @@ echo "dd if=/dev/nvme0n1 status=progress | bzip2 > /mnt/mdxfs/back-nvme0n1-`date
 chmod a+x backup-d.sh
 nohup ./backup-d.sh > backup-d.log 2>&1 &
 tail -f backup-d.log
+dd if=/dev/nvme0n1p3 status=progress | bzip2 > /mnt/mdxfs/back-nvme0n1p3-`date +%Y-%m-%d`.img.bz
+bzip2 -dc /mnt/mdxfs/back-nvme0n1p3-2022-11-29.img.bz | dd of=/dev/nvme0n1p3 status=progress
 echo "dd if=/dev/nvme0n1p3 status=progress | bzip2 > /mnt/mdxfs/back-nvme0n1p3-`date +%Y-%m-%d`.img.bz" > backup-p.sh
 chmod a+x backup-p.sh
 nohup ./backup-p.sh > backup-p.log 2>&1 &
+tail -f backup-p.log
 #mdubu
 dd if=/dev/sdc status=progress | bzip2 > /data0/back-sdc-`date +%Y-%m-%d`.img.bz
 bzip2 -cd /data0/back-sdc-2022-11-28.img.bz | dd of=/dev/sdc status=progress
